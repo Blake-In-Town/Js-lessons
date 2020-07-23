65c57806115f8563b9e0fdeb0a1d57b66e12ed2f
Blake-In-Town <boobies4size@gmail.com>
2020-07-23 16:15:42 +0300

    11.6.2 Работа с API
    Прикрутил стрелочные функции в промисы

--- ApiWork/ApiWork.js
+++ ApiWork/ApiWork.js
@@ -18,12 +18,11 @@ btn.addEventListener('click', function(ev) {
 
   window.fetch(url)
 
-    .then( function(response) {
+    .then( (response) => {
 
       if (response.status !== 200) {
         alert(
-          "Произошла ошибка при получении ответа от сервера:\n\n" +
-            response.message
+          "Произошла ошибка при получении ответа от сервера:\n\n" + response.message
         );
         return;
       }
@@ -32,14 +31,12 @@ btn.addEventListener('click', function(ev) {
       return response.json();
     })
 
-    .then( function(response) {
+    .then( (response) => {
 
       if (response.count == 0) {
         alert("К сожалению, данные не получены по запросу: " + search.value);
         return;
       }
-
-      console.log(response);
       
       let i = 0;
 
@@ -56,10 +53,6 @@ btn.addEventListener('click', function(ev) {
           liSearch[i].films = num.films.length;
 
           resultOfSearch.appendChild(liSearch[i]);
-          
-          console.log(num);
-          // console.log(liSearch[i].name);
-          // console.log(i);
 
           document.querySelector('.search_result').addEventListener('click', function(event) {
 
@@ -78,59 +71,3 @@ btn.addEventListener('click', function(ev) {
     });
  
 });
-
-
-
-
-
-
-// window.onload = function () {
-
-//   // window.fetch('data/test.json')
-//   //   .then( function(response) {
-//   //     console.log(response);
-//   //     return response.json();
-//   //   })
-//   //   .then( function() {
-//   //     console.log(response);
-//   //   })
-
-//   // Сохраняем адрес API
-//   // var api = "https://swapi.dev/api/";
-
-  
-//   //   // Формируем полный адрес запроса:
-//   //   var url = api + "people/?search="; // добавляем к запросу тип необходимых данных подробно о формате https://swapi.dev/documentation
-//   //   url += "obi"; // значение переменной запроса search
-  
-//   //   // Таким образом формируется строка вида:
-//   //   // https://swapi.dev/api/people/?search=obi
-  
-//   //   // Создаем объект XMLHttpRequest, при помощи которого будем отправлять запрос
-//   //   var request = new XMLHttpRequest();
-  
-//   //   // Назначаем обработчик события load для запроса
-//   //   request.addEventListener("load", function () {
-//   //     // отображаем в консоли текст ответа сервера
-//   //     console.log(request.response); 
-      
-//   //     // парсим его из JSON-строки в JavaScript-объект
-//   //     var response = JSON.parse(request.response); 
-
-      // Проверяем статус-код, который прислал сервер
-      // 200 — это ОК, остальные — ошибка или не подходят
-      
-
-//   //     // Проверяем, если поле имя в ответе на запрос
-      
-
-//   //     // Если все в порядке, то отображаем количество результатов поиска
-//   //     alert("Найдено персонажей:" + response.count);
-
-//   //   });
-
-//   //   // Обработчик готов, можно отправлять запрос
-//   //   // Открываем соединение и отправляем
-//   //   request.open("get", url);
-//   //   request.send();
-// };
